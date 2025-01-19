70262f2eb704541cfbb873d38a7a80affd450106
jmgv87191 <jmgv87191@gmail.com>
2025-01-18 20:22:13 -0700

    asd

--- src/app/components/ver-mas/ver-mas.component.ts
+++ src/app/components/ver-mas/ver-mas.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TomasService } from 'src/app/services/tomas.service';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { VerMas } from 'src/app/interfaces/tomas';
 import {MatListModule} from '@angular/material/list';
-import { IonItem, IonLabel, IonList, IonSpinner } from '@ionic/angular/standalone';
+import { IonItem, IonLabel, IonList, IonSpinner,IonHeader, IonToolbar, IonButtons,
+  IonMenuButton, IonTitle
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-ver-mas',
   templateUrl: './ver-mas.component.html',
   styleUrls: ['./ver-mas.component.scss'],
-  imports: [MatCardModule, MatButtonModule, MatListModule, RouterLink, IonItem, IonLabel, IonList, IonSpinner],
+  imports: [MatCardModule, MatButtonModule, MatListModule, RouterLink, IonItem, IonLabel, 
+    IonList, IonSpinner, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle],
 
 })
 export class VerMasComponent  implements OnInit {
@@ -32,7 +35,8 @@ export class VerMasComponent  implements OnInit {
 
   constructor(
     private aRoute: ActivatedRoute,
-    private tomasService: TomasService
+    private tomasService: TomasService,
+    private router: Router
   ) { 
 
     this.idToma = Number(this.aRoute.snapshot.paramMap.get('id'))
@@ -56,6 +60,10 @@ export class VerMasComponent  implements OnInit {
 
       this.loader = false
     })
+  }
+
+  regresar(){
+    this.router.navigate(['/dashboard'])
   }
 
 
